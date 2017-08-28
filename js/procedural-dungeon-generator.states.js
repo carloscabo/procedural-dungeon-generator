86303d94190d2d767ec9b5b94e16aba9ac6ada7a
Carlos Cabo <carloscabo@gmail.com>
2017-08-28 21:34:13 +0200

    Expanding statemachine

--- js/procedural-dungeon-generator.states.js
+++ js/procedural-dungeon-generator.states.js
@@ -5,16 +5,35 @@ var pDG = pDG || {};
 
 pDG.statemachine = {
 
+  // Array of elments, may be object better?
   states: null,
 
+  // Current stest
+  prev_idx: 0,
+  prev_state: null,
   current_idx: 0,
   current_state: null,
 
+  // You mus call this to initiaice the fSM
   init: function( states, initial_idx ) {
     this.states = states;
     this.len = this.states.length;
     if( typeof idx !== 'undefined' ) this.current_idx = initial_idx;
     this.current_state = this.states[ this.current_idx ];
+  },
+
+  next: function() {
+    var
+      _t = this;
+    // Console.log()
+    if ( _t.current_idx > 0 ) {
+      _t.current_idx--;
+      _t.current_state = _t.states[ _t.current_idx ];
+      _t.onChange( _t );
+      if ( _t.isLast() ) {
+        _t.onLast();
+      }
+    };
   },
 
   next: function() {
@@ -57,17 +76,20 @@ pDG.statemachine = {
   // You can override from outside
 
   onFirst: function() {
-
+    console.log( 'fSM: `'+this.current_state+'` {'+this.current_idx+'} is first!' );
   },
   onLast: function() {
-
+    console.log( 'fSM: `'+this.current_state+'` {'+this.current_idx+'} is last!' );
   },
   onChange: function() {
-
+    console.log( 'fSM: Changed from `'+this.prev_state+'` {'+this.prev_idx+'} to `'+this.current_state+'` {'+this.current_idx+'}' );
   }
 
 };
 
+/*
+  Predefined
+*/
 pDG.statemachine.init(
   [
   'generating-rooms',
